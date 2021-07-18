78ea818510def42339be369b070313828c57cca0
yunsang-jeong <imyunsang@kakao.com>
2021-07-18 16:56:49 +0900

    README와 example.tfvars 추가

--- 99-output.tf
+++ 99-output.tf
@@ -1,7 +1,9 @@
-output "eks_oidc_issuer" {
-  value = aws_eks_cluster.this.identity[0].oidc[0].issuer
+output "eks_cluster_id" {
+  description = "The id of the eks cluster"
+  value = aws_eks_cluster.this.id
 }
 
-output "eks_id" {
-  value = aws_eks_cluster.this.id
+output "eks_oidc_issuer" {
+  description = "The arn of the oidc issuer"
+  value = aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
